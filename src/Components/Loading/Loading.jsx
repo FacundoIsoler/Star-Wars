6c8c935727fc6f51cca186ac089db0d6dca526d1
Facundo Isoler <facundoisoler@outlook.com>
2023-04-18 12:42:46 -0300

    CSS LandingPage

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './Loading.module.css'
+import Joda from '../../Images/Joda.png'
 
 
 
@@ -9,6 +10,7 @@ export default function Loading() {
     return (
     
         <div className={styles.Page}>
+            <img src={Joda}  className={styles.Image} alt=''/>
             <h1 className={styles.Loading}>Patience you must have, my young padawan</h1>
         </div>
     )
